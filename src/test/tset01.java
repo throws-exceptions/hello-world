315f6168b9a99aea1f815ebc8e472fbb6c064527
Mr.F <Mr.F@DESKTOP-GOESFET>
2019-02-01 20:01:42 +0800

    这是第二次测试

--- src/test/tset01.java
+++ src/test/tset01.java
@@ -4,7 +4,7 @@ public class tset01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("²âÊÔ°æ±¾¿ØÖÆ");
+		System.out.println("ç¬¬äºŒæ¬¡æµ‹è¯•ç‰ˆæœ¬æ§åˆ¶");
 	}
 
 }
